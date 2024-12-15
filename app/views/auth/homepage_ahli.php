163abff57ef0626b8a767443bdd094a42ce18c71
Harini Sangaran <harini04@graduate.utm.my>
2024-12-15 22:56:46 +0800

    member homepage

---
+++ app/views/auth/homepage_ahli.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <title> KADA Homepage </title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    </head>
+    <body style="font-family:comic sans ms; background-color: lavender;">
+
+        <nav class="navbar navbar-expand-md fixed-top navbar-light bg-warning">
+            <div class="container-xxl">
+                <a href="/homepageAhli" class="navbar-brand">
+                    <span class="fw-bold text-dark">
+                        Koperasi Kakitangan KADA         
+                    </span>
+                </a>
+
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#main-nav" aria-controls="main-nav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse justify-content-end align-center" id="main-nav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link text-dark" href="register.php"> Mohon Pinjaman&nbsp;&nbsp;&nbsp;&nbsp; </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-dark" href=""> Semak Kelulusan Pinjaman&nbsp;&nbsp;&nbsp;&nbsp; </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-dark" href=""> Semak Simpanan&nbsp;&nbsp;&nbsp;&nbsp; </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-dark" href=""> Baki Pinjaman&nbsp;&nbsp;&nbsp;&nbsp; </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-dark" href=""> Semak Penyata&nbsp;&nbsp;&nbsp;&nbsp; </a>
+                        </li>
+                        <li class="nav-item d-md-none">
+                            <a class="nav-link text-dark" href="/logout"> Logout </a>
+                        </li>
+                        <li class="nav-item m-2 d-none d-md-inline">
+                            <a class="btn btn-sm btn-info text-dark" href="/logout"> Logout </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <div class="container py-5 text-center"></div>
+        <a href="" class="btn btn-primary mb-2">Mohon Pinjaman</a>
+        <a href="" class="btn btn-primary mb-2">Semak Kelulusan Pinjaman</a>
+        <a href="" class="btn btn-primary">Semak Simpanan</a>
+        <a href="" class="btn btn-primary mb-2">Baki Pinjaman</a>
+        <a href="" class="btn btn-primary mb-2">Semak Penyata</a>
+    </div>
+</body>
+</html>
