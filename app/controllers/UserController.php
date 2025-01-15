0c3e17f0579581aaa2b356bdae4f44b3fd428366
ikaazhar <ikasyafinyazhar@gmail.com>
2025-01-15 13:22:13 +0800

    tukar role

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -53,6 +53,7 @@ class UserController extends Controller
         if ($user && password_verify($password, $user['password'])) {
             session_start();
             $_SESSION['user_id'] = $user['member_id'];
+            $_SESSION['role'] = $user['user_role'];
             header('Location: /homepageMember');
         } else {
             echo "<h3 style='color: red;'>Invalid email or password. Please try again.</h3>";
@@ -70,6 +71,7 @@ class UserController extends Controller
         if ($user && password_verify($password, $user['password'])) {
             session_start();
             $_SESSION['user_id'] = $user['admin_id'];
+            $_SESSION['role'] = $user['user_role'];
             //$_SESSION['user_name'] = $user['name'];
             header('Location: /homepageAdmin');
         } else {
@@ -88,6 +90,7 @@ class UserController extends Controller
         if ($user && password_verify($password, $user['password'])) {
             session_start();
             $_SESSION['user_id'] = $user['ALK_id'];
+            $_SESSION['role'] = $user['user_role'];
             //$_SESSION['user_name'] = $user['name'];
             header('Location: /homepageALK');
         } else {
--- public/index.php
+++ public/index.php
@@ -25,221 +25,256 @@ $uri = explode('?', trim($_SERVER['REQUEST_URI'], '/'))[0];
 $action = isset($_GET['action']) ? $_GET['action'] : 'calendar';
 $method = $_SERVER['REQUEST_METHOD'];
 
-if ($uri === '' && $method === 'GET') {
-    $controller->homepage();
-} elseif ($uri === 'homepage' && $method === 'GET') {
-    $controller->homepage();
-} elseif ($uri === 'buttonpage' && $method === 'GET') {
-    $controller->buttonpage();
-} elseif ($uri === 'register' && $method === 'POST') {
-    $controller->register();
-} elseif ($uri === 'loginMember' && $method === 'GET') {
-    $controller->loginMember();
-} elseif ($uri === 'loginStaff' && $method === 'GET') {
-    $controller->loginStaff();
-} elseif ($uri === 'loginALK' && $method === 'GET') {
-    $controller->loginALK();
-} elseif ($uri === 'createMember' && $method === 'GET') {
-    $controller->createMember();
-} elseif ($uri === 'createAdmin' && $method === 'GET') {
-    $controller->createAdmin();
-} elseif ($uri === 'createALK' && $method === 'GET') {
-    $controller->createALK();
-} elseif ($uri === 'authenticateMember' && $method === 'POST') {
-    $controller->authenticateMember();
-} elseif ($uri === 'authenticateStaff' && $method === 'POST') {
-    $controller->authenticateStaff();
-} elseif ($uri === 'authenticateALK' && $method === 'POST') {
-    $controller->authenticateALK();
-} elseif ($uri === 'logout' && $method === 'GET') {
-    $controller->logout();
-} elseif ($uri === 'create' && $method === 'GET') {
-    $controller->create();
-} elseif ($uri === 'createMembershipForm' && $method === 'GET') {
-    $controller->createMembershipForm();
-} elseif ($uri === 'storeMembershipForm' && $method === 'POST') {
-    $controller->storeMembershipForm(); 
-} elseif ($uri === 'store' && $method === 'POST') {
-    $controller->store();
-} elseif ($uri === 'checkAcc' && $method === 'GET') {
-    $controller->showCheckAccountForm();
-} elseif ($uri === 'getAccInfo' && $method === 'POST') {
-    $controller->checkAccount();
-} elseif (preg_match('/edit\/(\d+)/', $uri, $matches) && $method === 'GET') {
-    if (isAuthenticated()) {
-        $controller->edit($matches[1]);
+if (isset($_SESSION['role']) && $_SESSION['role'] == "ALK"){
+    if ($uri === 'homepageALK' && $method === 'GET') {
+        if (isAuthenticated()) {
+            $controller->homepageALK();
+        } else {
+            $controller->homepage();
+        }
+    } elseif ($uri === 'approveMembershipForm' && $method === 'GET') {
+        if (isAuthenticated()) {
+            $controller->approveMembershipForm();
+        } else {
+            $controller->homepage();
+        }
+    } elseif ($uri === 'updateMembershipFormStatusALK' && $method === 'POST') {
+        if (isAuthenticated()) {
+            $controller->updateMembershipFormStatusALK($_POST);
+        } else {
+            $controller->homepage();
+        }
+    } elseif ($uri === 'listReviewedLoan' && $method === 'GET') {
+        if (isAuthenticated()) {
+            $controller->listReviewedLoanApp();
+        } else {
+            $controller->homepage();
+        }
+    } elseif ($uri === 'updateLoanApproval' && $method === 'POST') {
+        if (isAuthenticated()) {
+            $controller->LoanApproval();
+        } else {
+            $controller->homepage();
+        }
+    } elseif ($uri === 'logout' && $method === 'GET') {
+        $controller->logout();
+    } elseif ($uri === 'viewMembershipForm' && $method === 'GET') {
+        if (isAuthenticated()) {
+            $controller->viewMembershipForm($_GET['id_number']);
+        } else {
+            $controller->homepage();
+        }
+    } elseif ($uri === 'viewLoanApplication' && $method === 'GET') {
+        if (isAuthenticated()) {
+            $controller->viewLoanApplication($_GET['loan_id']);
+        } else {
+            $controller->homepage();
+        }
+    } elseif ($action == 'calendar') {
+        if (isAuthenticated()) {
+            $controller->calendar();
+        } else {
+            $controller->homepage();
+        }
     } else {
-        $controller->homepage();
+        http_response_code(404);
+        echo "Page not found.";
     }
-} elseif (preg_match('/update\/(\d+)/', $uri, $matches) && $method === 'POST') {
-    if (isAuthenticated()) {
-        $controller->update($matches[1]);
+}
+else if (isset($_SESSION['role']) && $_SESSION['role'] == "MEMBER"){
+    if ($uri === 'homepageMember' && $method === 'GET') {
+        if (isAuthenticated()) {
+            $controller->homepageMember();
+        } else {
+            $controller->homepage();
+        }
+    } elseif ($uri === 'viewInvoice' && $method === 'GET') {
+        if (isAuthenticated()) {
+            $controller->showInvoice();
+        } else {
+            $controller->homepage();
+        }
+    } elseif ($uri === 'viewSaving' && $method === 'GET') {
+        if (isAuthenticated()) {
+            $controller->viewSaving();
+        } else {
+            $controller->homepage();
+        }
+    } elseif ($uri === 'loanInfo' && $method === 'GET') {
+        if (isAuthenticated()) {
+            $controller->loanInfo();
+        } else {
+            $controller->homepage();
+        }
+    }  elseif ($uri === 'viewLoanForm' && $method === 'GET') {
+        if (isAuthenticated()) {
+            $controller->viewLoanForm();
+        } else {
+            $controller->homepage();
+        }
+    } elseif ($uri === 'download_loan_template' && $method === 'GET') {
+        if (isAuthenticated()) {
+            $controller->downloadLoanTemplate();
+        } else {
+            $controller->homepage();
+        }
+    } elseif ($uri === 'submitLoanApp' && $method === 'POST') {
+        if (isAuthenticated()) {
+            $controller->submitLoanApplication($_POST);
+        } else {
+            $controller->homepage();
+        }
+    } elseif ($uri === 'loanStatus' && $method === 'GET') {
+        if (isAuthenticated()) {
+            $controller->loanStatus();
+        } else {
+            $controller->homepage();
+        }
+    } elseif ($uri === 'loanBalance' && $method === 'GET') {
+        if (isAuthenticated()) {
+            $controller->loanBalance();
+        } else {
+            $controller->homepage();
+        }
+    } elseif ($uri === 'showSaving' && $method === 'GET') {
+        if (isAuthenticated()) {
+            $controller->showSavings();
+        } else {
+            $controller->homepage();
+        }
+    } elseif ($uri == 'showProfile'&& $method === 'GET') {
+        if (isAuthenticated()) {
+            $controller->showProfile();
+        } else {
+            $controller->homepage();
+        }
+    } elseif ($uri === 'logout' && $method === 'GET') {
+        $controller->logout();
     } else {
-        $controller->homepage();
+        http_response_code(404);
+        echo "Page not found.";
     }
-} elseif (preg_match('/delete\/(\d+)/', $uri, $matches) && $method === 'POST') {
-    if (isAuthenticated()) {
-        $controller->delete($matches[1]);
-    } else {
-        $controller->homepage();
-    }
-} elseif ($uri === 'homepageMember' && $method === 'GET') {
-    if (isAuthenticated()) {
-        $controller->homepageMember();
-    } else {
-        $controller->homepage();
-    }
-} elseif ($uri === 'homepageAdmin' && $method === 'GET') {
-    if (isAuthenticated()) {
-        $controller->homepageAdmin();
-    } else {
-        $controller->homepage();
-    }
-} elseif ($uri === 'homepageALK' && $method === 'GET') {
-    if (isAuthenticated()) {
-        $controller->homepageALK();
-    } else {
-        $controller->homepage();
-    }
-} elseif ($uri === 'viewInvoice' && $method === 'GET') {
-    if (isAuthenticated()) {
-        $controller->showInvoice();
-    } else {
-        $controller->homepage();
-    }
-} elseif ($uri === 'loanInfo' && $method === 'GET') {
-    if (isAuthenticated()) {
-        $controller->loanInfo();
-    } else {
-        $controller->homepage();
-    }
-} elseif ($uri === 'viewSaving' && $method === 'GET') {
-    if (isAuthenticated()) {
-        $controller->viewSaving();
-    } else {
-        $controller->homepage();
-    }
-} elseif ($uri === 'viewLoanForm' && $method === 'GET') {
-    if (isAuthenticated()) {
-        $controller->viewLoanForm();
-    } else {
-        $controller->homepage();
-    }
-} elseif ($uri === 'download_loan_template' && $method === 'GET') {
-    if (isAuthenticated()) {
-        $controller->downloadLoanTemplate();
-    } else {
-        $controller->homepage();
-    }
-} elseif ($uri === 'submitLoanApp' && $method === 'POST') {
-    if (isAuthenticated()) {
-        $controller->submitLoanApplication($_POST);
-    } else {
-        $controller->homepage();
-    }
-} elseif ($uri === 'loanStatus' && $method === 'GET') {
-    if (isAuthenticated()) {
-        $controller->loanStatus();
-    } else {
-        $controller->homepage();
-    }
-} elseif ($uri === 'loanBalance' && $method === 'GET') {
-    if (isAuthenticated()) {
-        $controller->loanBalance();
-    } else {
-        $controller->homepage();
-    }
-} elseif ($uri === 'listPendingForm' && $method === 'GET') {
-    if (isAuthenticated()) {
-        $controller->listPendingApplications();
-    } else {
-        $controller->homepage();
-    }
-} elseif ($uri === 'updateLoanStatus' && $method === 'POST') {
-    if (isAuthenticated()) {
-        $controller->updateLoanStatus();
-    } else {
-        $controller->homepage();
-    }
-} elseif ($uri === 'viewLoanApplication' && $method === 'GET') {
-    if (isAuthenticated()) {
-        $controller->viewLoanApplication($_GET['loan_id']);
-    } else {
-        $controller->homepage();
-    }
-} elseif ($uri === 'reviewMembershipForm' && $method === 'GET') {
-    if (isAuthenticated()) {
-        $controller->reviewMembershipForm();
-    } else {
-        $controller->homepage();
-    }
-} elseif ($uri === 'storeMembershipFormStatus' && $method === 'POST') {
-    if (isAuthenticated()) {
-        $controller->storeMembershipFormStatus($_POST);
-    } else {
-        $controller->homepage();
-    }
-} elseif ($uri === 'viewMembershipForm' && $method === 'GET') {
-    if (isAuthenticated()) {
-        $controller->viewMembershipForm($_GET['id_number']);
-    } else {
-        $controller->homepage();
-    }
-} elseif ($uri === 'approveMembershipForm' && $method === 'GET') {
-    if (isAuthenticated()) {
-        $controller->approveMembershipForm();
-    } else {
-        $controller->homepage();
-    }
-} elseif ($uri === 'updateMembershipFormStatusALK' && $method === 'POST') {
-    if (isAuthenticated()) {
-        $controller->updateMembershipFormStatusALK($_POST);
-    } else {
-        $controller->homepage();
-    }
-} elseif ($uri === 'listReviewedLoan' && $method === 'GET') {
-    if (isAuthenticated()) {
-        $controller->listReviewedLoanApp();
-    } else {
-        $controller->homepage();
-    }
-} elseif ($uri === 'updateLoanApproval' && $method === 'POST') {
-    if (isAuthenticated()) {
-        $controller->LoanApproval();
-    } else {
-        $controller->homepage();
-    }
-} elseif ($uri === 'showSaving' && $method === 'GET') {
-    if (isAuthenticated()) {
-        $controller->showSavings();
-    } else {
-        $controller->homepage();
-    }
-} elseif ($uri == 'showProfile'&& $method === 'GET') {
-    if (isAuthenticated()) {
-        $controller->showProfile();
-    } else {
-        $controller->homepage();
-    }
-} elseif ($uri == 'editPersonalInfo'&& $method === 'GET') {
-    if (isAuthenticated()) {
-        $controller->editPersonalInfo();
-    } else {
-        $controller->homepage();
-    }
-} elseif ($uri == 'updateMemberInfo'&& $method === 'POST') {
-    if (isAuthenticated()) {
-        $controller->updateMemberInfo();
-    } else {
-        $controller->homepage();
-    }
-} elseif ($action == 'calendar') {
-     if (isAuthenticated()) {
-         $controller->calendar();
-     } else {
-         $controller->homepage();
-     }
-}  else {
+
+}
+
+elseif (isset($_SESSION['role']) && $_SESSION['role'] == "ADMIN"){
+    if ($uri === 'homepageAdmin' && $method === 'GET') {
+        if (isAuthenticated()) {
+            $controller->homepageAdmin();
+        } else {
+            $controller->homepage();
+        }
+    } elseif ($uri === 'listPendingForm' && $method === 'GET') {
+        if (isAuthenticated()) {
+            $controller->listPendingApplications();
+        } else {
+            $controller->homepage();
+        }
+    } elseif ($uri === 'updateLoanStatus' && $method === 'POST') {
+        if (isAuthenticated()) {
+            $controller->updateLoanStatus();
+        } else {
+            $controller->homepage();
+        }
+    } elseif ($uri === 'viewLoanApplication' && $method === 'GET') {
+        if (isAuthenticated()) {
+            $controller->viewLoanApplication($_GET['loan_id']);
+        } else {
+            $controller->homepage();
+        }
+    } elseif ($uri === 'reviewMembershipForm' && $method === 'GET') {
+        if (isAuthenticated()) {
+            $controller->reviewMembershipForm();
+        } else {
+            $controller->homepage();
+        }
+    } elseif ($uri === 'storeMembershipFormStatus' && $method === 'POST') {
+        if (isAuthenticated()) {
+            $controller->storeMembershipFormStatus($_POST);
+        } else {
+            $controller->homepage();
+        }
+    } elseif ($uri === 'viewMembershipForm' && $method === 'GET') {
+        if (isAuthenticated()) {
+            $controller->viewMembershipForm($_GET['id_number']);
+        } else {
+            $controller->homepage();
+        }
+    } elseif ($uri === 'logout' && $method === 'GET') {
+        $controller->logout();
+    } elseif ($action == 'calendar') {
+        if (isAuthenticated()) {
+            $controller->calendar();
+        } else {
+            $controller->homepage();
+        }
+   } else {
     http_response_code(404);
     echo "Page not found.";
+}
+    
+}
+
+else if(!isset($_SESSION['role'])){
+    if ($uri === '' && $method === 'GET') {
+        $controller->homepage();
+    } elseif ($uri === 'homepage' && $method === 'GET') {
+        $controller->homepage();
+    } elseif ($uri === 'buttonpage' && $method === 'GET') {
+        $controller->buttonpage();
+    } elseif ($uri === 'register' && $method === 'POST') {
+        $controller->register();
+    } elseif ($uri === 'loginMember' && $method === 'GET') {
+        $controller->loginMember();
+    } elseif ($uri === 'loginStaff' && $method === 'GET') {
+        $controller->loginStaff();
+    } elseif ($uri === 'loginALK' && $method === 'GET') {
+        $controller->loginALK();
+    } elseif ($uri === 'createMember' && $method === 'GET') {
+        $controller->createMember();
+    } elseif ($uri === 'createAdmin' && $method === 'GET') {
+        $controller->createAdmin();
+    } elseif ($uri === 'createALK' && $method === 'GET') {
+        $controller->createALK();
+    } elseif ($uri === 'authenticateMember' && $method === 'POST') {
+        $controller->authenticateMember();
+    } elseif ($uri === 'authenticateStaff' && $method === 'POST') {
+        $controller->authenticateStaff();
+    } elseif ($uri === 'authenticateALK' && $method === 'POST') {
+        $controller->authenticateALK();
+    } elseif ($uri === 'logout' && $method === 'GET') {
+        $controller->logout();
+    } elseif ($uri === 'create' && $method === 'GET') {
+        $controller->create();
+    } elseif ($uri === 'createMembershipForm' && $method === 'GET') {
+        $controller->createMembershipForm();
+    } elseif ($uri === 'storeMembershipForm' && $method === 'POST') {
+        $controller->storeMembershipForm(); 
+    } elseif ($uri === 'store' && $method === 'POST') {
+        $controller->store();
+    } elseif ($uri === 'checkAcc' && $method === 'GET') {
+        $controller->showCheckAccountForm();
+    } elseif ($uri === 'getAccInfo' && $method === 'POST') {
+        $controller->checkAccount();
+    } elseif (preg_match('/edit\/(\d+)/', $uri, $matches) && $method === 'GET') {
+        if (isAuthenticated()) {
+            $controller->edit($matches[1]);
+        } else {$controller->homepage();
+        }
+    } elseif (preg_match('/update\/(\d+)/', $uri, $matches) && $method === 'POST') {
+        if (isAuthenticated()) {
+            $controller->update($matches[1]);
+        } else {
+            $controller->homepage();
+        }
+    } elseif (preg_match('/delete\/(\d+)/', $uri, $matches) && $method === 'POST') {
+        if (isAuthenticated()) {
+            $controller->delete($matches[1]);
+        } else {
+            $controller->homepage();
+        }
+    } else {
+        http_response_code(404);
+        echo "Page not found.";
+    }
 }
